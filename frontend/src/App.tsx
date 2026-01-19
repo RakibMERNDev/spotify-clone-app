80e4f37d1f55a515a1a5f23a0fbf7813077994b6
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-19 16:09:58 +0600

    initial project setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hello</h1>
+      <Button variant={"secondary"}>This is a button</Button>
+    </>
+  );
+}
+
+export default App;
