76433f3fa735a3fd4ea63e1b32204c9f9821c45f
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-19 16:55:16 +0600

    added clerk auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import { Button } from "./components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello</h1>
-      <Button variant={"secondary"}>This is a button</Button>
-    </>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   );
 }
 
