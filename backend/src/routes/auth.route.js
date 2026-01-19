e7632f70d76aa2bfa3c388856764cb00e1c26fef
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-20 02:28:05 +0600

    added auth controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,29 +1,8 @@
 import { Router } from "express";
-import { User } from "../models/user.model.js";
+import { authCallback } from "../controller/auth.controller.js";
 
 const router = Router();
 
-router.post("/callback", async (req, res) => {
-  try {
-    const { id, firstName, lastName, imageUrl } = req.body;
-
-    // if user already exist
-
-    const user = await User.findOne({ clerkId: id });
-
-    if (!user) {
-      // signup
-      await User.create({
-        clerkId: id,
-        fullName: `${firstName} ${lastName}`,
-        imageUrl,
-      });
-    }
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.log("Error in auth Callback", error);
-    res.status(500).json({ success: false });
-  }
-});
+router.post("/callback", authCallback);
 
 export default router;
