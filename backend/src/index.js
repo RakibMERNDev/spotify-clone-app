b2cbe733ad7fe7987bb30b2fcc08599ced8229f9
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-19 23:15:11 +0600

    added mongodb connection to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -26,4 +27,5 @@ app.use("/api/stats", statRoutes);
 
 app.listen(5000, () => {
   console.log(`Server is running on port: ` + `http://localhost:${PORT}`);
+  connectDB();
 });
---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log(`Connected to MongoDB: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
+
+    process.exit(1);
+  }
+};
