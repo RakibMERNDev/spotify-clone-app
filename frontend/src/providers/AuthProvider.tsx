fe31402e45691b391cad3f4d40d6010409cb3447
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-22 17:43:32 +0600

    refactor AuthProvider to use useEffect instead of useEffectEvent for better compatibility

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -2,23 +2,22 @@ import { axiosInstance } from "@/lib/axios";
 import { useAuth } from "@clerk/clerk-react";
 import { Loader } from "lucide-react";
 
-import { useEffectEvent, useState } from "react";
+import {  useEffect, useState } from "react";
+
+const updateApiToken = (token: string | null) => {
+  if (token) {
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axiosInstance.defaults.headers.common["Authorization"];
+  }
+};
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const updateApiToken = (token: string | null) => {
-    if (token) {
-      axiosInstance.defaults.headers.common["Authorization"] =
-        `Bearer ${token}`;
-    } else {
-      delete axiosInstance.defaults.headers.common["Authorization"];
-    }
-  };
-
   const { getToken } = useAuth();
 
   const [loading, setLoading] = useState(true);
 
-  useEffectEvent(() => {
+  useEffect(() => {
     const initAuth = async () => {
       try {
         const token = await getToken();
