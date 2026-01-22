3cbfc4af599ea081409c1bda5d070e511ff97ce7
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-22 17:58:43 +0600

    add TopBar and SignInOauthButtons components; update HomePage to include TopBar

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,9 +1,11 @@
-
+import TopBar from "@/components/ui/TopBar";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <TopBar />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
