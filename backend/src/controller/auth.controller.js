6f3669885161c32f10f3d3fb7be1649a8d4b3bc0
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-25 15:31:42 +0600

    implement auth callback functionality and user synchronization; add Card component for UI consistency

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,24 +1,24 @@
 import { User } from "../models/user.model.js";
 
 export const authCallback = async (req, res, next) => {
-  try {
-    const { id, firstName, lastName, imageUrl } = req.body;
+	try {
+		const { id, firstName, lastName, imageUrl } = req.body;
 
-    // if user already exist
+		// check if user already exists
+		const user = await User.findOne({ clerkId: id });
 
-    const user = await User.findOne({ clerkId: id });
+		if (!user) {
+			// signup
+			await User.create({
+				clerkId: id,
+				fullName: `${firstName || ""} ${lastName || ""}`.trim(),
+				imageUrl,
+			});
+		}
 
-    if (!user) {
-      // signup
-      await User.create({
-        clerkId: id,
-        fullName: `${firstName} ${lastName}`,
-        imageUrl,
-      });
-    }
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.log("Error in auth Callback", error);
-    next(error);
-  }
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.log("Error in auth callback", error);
+		next(error);
+	}
 };
